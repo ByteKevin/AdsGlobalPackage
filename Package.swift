e5a519d712fb82b54d561552775eb16e83c065fe
makaiwen <makaiwen@bytedance.com>
2025-10-21 20:54:52 +0800

    test

--- Package.swift
+++ Package.swift
@@ -10,25 +10,25 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AdsGlobalPackage",
-            targets: ["PAGAdSDK", "TikTokBusinessSDK"]
+            targets: ["PAGAdSDK"]
         ),
         .library(
-            name: "PAGAdSDK",
-            targets: ["PAGAdSDK"]
+            name: "TikTokBusinessSDK",
+            targets: ["TikTokBusinessSDK"]
         ),
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "PAGAdSDKFramework",
+        .binaryTarget(name: "PAGAdSDK",
                       url: "https://sf16-fe-tos-sg.i18n-pglstatp.com/obj/pangle-sdk-static-va/7.8.0.0/PAGAdSDK.xcframework.zip",
                       checksum: "705e6156c02e73d3aca475375492c45ff96f591d248589a8c3faeb6443317b86"),
         .binaryTarget(name: "TikTokBusinessSDKFramework",
                       url: "https://sf16-fe-tos-sg.i18n-pglstatp.com/obj/pangle-sdk-static-va/7.8.0.0/TikTokBusinessSDK.xcframework.zip",
                       checksum: "038da6e36f7c0a8a6dbcd8f3750932f2cfc8eb1dc14f5f7f3c488a956d3f61bf"),
         .target(
-            name: "PAGAdSDK",
+            name: "AdsGlobalPackage",
             dependencies: [
-                .target(name: "PAGAdSDKFramework"),
+                .target(name: "PAGAdSDK"),
             ],
             resources: [.copy("PAGAdSDK.bundle")],
             linkerSettings: [
@@ -65,30 +65,9 @@ let package = Package(
             name: "TikTokBusinessSDK",
             dependencies: [
                 .target(name: "TikTokBusinessSDKFramework"),
-                .target(name: "PAGAdSDK"),
+                .target(name: "AdsGlobalPackage"),
             ],
-            linkerSettings: [
-                .linkedFramework("UIKit"),
-                .linkedFramework("WebKit"),
-                .linkedFramework("MediaPlayer"),
-                .linkedFramework("AdSupport"),
-                .linkedFramework("CoreMedia"),
-                .linkedFramework("AVFoundation"),
-                .linkedFramework("CoreTelephony"),
-                .linkedFramework("StoreKit"),
-                .linkedFramework("SystemConfiguration"),
-                .linkedFramework("MobileCoreServices"),
-                .linkedFramework("CoreMotion"),
-                .linkedFramework("Accelerate" ),
-                .linkedFramework("AudioToolbox"),
-                .linkedFramework("JavaScriptCore"),
-                .linkedFramework("CoreGraphics"),
-                .linkedFramework("Security"),
-                .linkedFramework("DeviceCheck"),
-                .linkedFramework("CoreML"),
-                .linkedFramework("AppTrackingTransparency"),
-                .linkedLibrary("z"),
-            ]
+            linkerSettings: []
         )
     ]
 )
