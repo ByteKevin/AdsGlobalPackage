ce2c60c4aa81df5e88e939da29ef66396ef26fbf
makaiwen <makaiwen@bytedance.com>
2025-10-22 15:44:42 +0800

    Update to v7.8.0.0

--- Package.swift
+++ Package.swift
@@ -4,32 +4,31 @@
 import PackageDescription
 
 let package = Package(
-    name: "AdsGlobalPackage",
+    name: "AdsGlobal",
     defaultLocalization: "en",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AdsGlobalPackage",
-            targets: ["PAGAdSDK", "TikTokBusinessSDK"]),
+            targets: ["AdsGlobalPackage"]),
         .library(
-            name: "PAGAdSDK",
-            targets: ["PAGAdSDK"]),
+            name: "TikTokBusinessSDK",
+            targets: ["TikTokBusinessSDK", "AdsGlobalPackage"]),
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "PAGAdSDKFramework",
+        .binaryTarget(name: "PAGAdSDK",
                       url: "https://sf16-fe-tos-sg.i18n-pglstatp.com/obj/pangle-sdk-static-va/7.8.0.0/PAGAdSDK.xcframework.zip",
                       checksum: "705e6156c02e73d3aca475375492c45ff96f591d248589a8c3faeb6443317b86"),
         .binaryTarget(name: "TikTokBusinessSDK",
                       url: "https://sf16-fe-tos-sg.i18n-pglstatp.com/obj/pangle-sdk-static-va/7.8.0.0/TikTokBusinessSDK.xcframework.zip",
                       checksum: "038da6e36f7c0a8a6dbcd8f3750932f2cfc8eb1dc14f5f7f3c488a956d3f61bf"),
         .target(
-            name: "PAGAdSDK",
+            name: "AdsGlobalPackage",
             dependencies: [
-                .target(name: "PAGAdSDKFramework"),
+                .target(name: "PAGAdSDK"),
             ],
             resources: [.copy("PAGAdSDK.bundle")],
-            
             linkerSettings: [
                 .linkedFramework("UIKit"),
                 .linkedFramework("WebKit"),
@@ -58,7 +57,9 @@ let package = Package(
                 .linkedLibrary("iconv"),
                 .linkedLibrary("c++abi"),
                 .linkedLibrary("c++"),
-            ]
-        ),
+            ]),
+        .testTarget(
+            name: "AdsGlobalPackageTests",
+            dependencies: ["AdsGlobalPackage"]),
     ]
 )
