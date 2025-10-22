ff69ef997b06fd98e7f1a5ccd46b500dcf2c32f1
makaiwen <makaiwen@bytedance.com>
2025-10-22 15:52:44 +0800

    Update to v7.8.0.0

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "AdsGlobal",
+    name: "AdsGlobalPackage",
     defaultLocalization: "en",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
